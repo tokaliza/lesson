acd94f82c0a299a473db956ce37f2c4e9e969296
Hanna Tompson <lizatokarr@gmail.com>
2023-11-17 12:39:42 +0200

    first commit

---
+++ my-app/src/Component.js
@@ -0,0 +1,12 @@
+import img from './assets/img/tvshow.avif';
+
+function Component () {
+    return (
+        <div className="content">
+            <img src={img} alt="tv show" />
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+export default Component;
